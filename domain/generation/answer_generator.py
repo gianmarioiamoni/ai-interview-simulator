9148620d744a4f64b878037add1fcdf401569dbf
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-02-23 02:43:09 +0100

    remove answer_generator.py dummy file

--- domain/generation/answer_generator.py
+++
@@ -1,9 +0,0 @@
-# Answer generation logic
-
-from typing import List
-from langchain.schema import Document
-
-class AnswerGenerator:
-    def generate(self, query: str, documents: List[Document]) -> str:
-        # Placeholder: chain LangChain
-        return "Not implemented yet"
