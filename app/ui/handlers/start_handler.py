71fbbdb2fb2497430c14e1ea4e40d66c08b31102
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-03-11 13:43:18 +0100

    fix arg names in start_interview

--- app/ui/handlers/start_handler.py
+++ app/ui/handlers/start_handler.py
@@ -1,9 +1,11 @@
 # app/ui/handlers/start_handler.py
 
 from app.ui.state_handlers import start_interview
+from domain.contracts.interview_type import InterviewType
+from domain.contracts.role import RoleType
 
 
-def start_handler(role, interview_type, company, language):
+def start_handler(role: RoleType, interview_type: InterviewType, company: str, language: str):
 
     response = start_interview(
         role=role,
