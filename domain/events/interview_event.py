a926c43e81c7ba3081ffa1fa6d5f96c527c61818
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-03-15 05:15:21 +0100

    fix timezone import in interview_event.py

--- domain/events/interview_event.py
+++ domain/events/interview_event.py
@@ -1,11 +1,10 @@
 # domain/events/interview_event.py
 
-from pydantic import BaseModel
-from datetime import datetime
-from pytz import timezone
+from pydantic import BaseModel, Field
+from datetime import datetime, timezone
 
 
 class InterviewEvent(BaseModel):
 
     type: str
-    timestamp: datetime = datetime.now(timezone.utc)
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
