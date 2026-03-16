b91059dde59947015fa637fc0f10a6801bae804d
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-03-16 13:02:56 +0100

    add log at startup

--- gradio_app.py
+++ gradio_app.py
@@ -6,6 +6,7 @@ from app.ui.app import build_app
 if not os.getenv("OPENAI_API_KEY"):
     print("WARNING: OPENAI_API_KEY not set")
 
+print("BUILD VERSION: 2026-03-16-A")
 app = build_app()
 
 app.launch(server_name="0.0.0.0", server_port=7860)
