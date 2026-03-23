410aa943f8f9585fa4ba9617a37518d2f8c0395c
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-03-23 13:32:32 +0100

    restore original app.py

--- app/ui/app.py
+++ app/ui/app.py
@@ -2,32 +2,28 @@
 
 import gradio as gr
 
-# from app.ui.layout.ui_layout import build_layout
-# from app.ui.bindings.ui_bindings import bind_events
+from app.ui.layout.ui_layout import build_layout
+from app.ui.bindings.ui_bindings import bind_events
 
-# def build_app():
-
-#     with gr.Blocks(
-#         css="""
-#             #code-editor textarea {
-#                 font-family: monospace;
-#                 font-size: 14px;
-#             }
-
-#             #code-editor .cm-content {
-#                 font-family: monospace;
-#                 font-size: 14px;
-#             }
-#         """
-#     ) as demo:
-
-#         components = build_layout()
-
-#         bind_events(components=components)
-
-
-#     return demo
 def build_app():
-    with gr.Blocks() as app:
-        gr.Markdown("Test OK")
-    return app
+
+    with gr.Blocks(
+        css="""
+            #code-editor textarea {
+                font-family: monospace;
+                font-size: 14px;
+            }
+
+            #code-editor .cm-content {
+                font-family: monospace;
+                font-size: 14px;
+            }
+        """
+    ) as demo:
+
+        components = build_layout()
+
+        bind_events(components=components)
+
+
+    return demo
