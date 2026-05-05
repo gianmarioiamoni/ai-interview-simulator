cd1b29a7164255ca800d9ff35802694f20624a5f
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-05-05 20:36:46 +0200

    fix(ui): align UIOutputsBuilder with updated UIResponse contract after container removal

--- app/ui/bindings/builders/ui_outputs_builder.py
+++ app/ui/bindings/builders/ui_outputs_builder.py
@@ -7,30 +7,34 @@ class UIOutputsBuilder:
 
     def build(self):
         return [
+            # STATE
             self.c.state,
-            # container switch
-            self.c.setup_container,
-            self.c.interview_container,
-            # setup inputs (values preserved)
+            # SETUP INPUTS
             self.c.role_input,
             self.c.interview_type_input,
             self.c.company_input,
             self.c.language_input,
             self.c.start_button,
+            # TITLE
             self.c.page_title,
-            # interview UI
+            # HEADER
             self.c.question_counter,
             self.c.feedback_output,
+            # DISPLAY
             self.c.written_display,
             self.c.coding_display,
             self.c.database_display,
+            # REPORT
             self.c.final_feedback,
             self.c.report_output,
+            # BUTTONS
             self.c.submit_button,
             self.c.retry_button,
             self.c.next_button,
+            # EDITORS
             self.c.written_box,
             self.c.coding_box,
             self.c.database_box,
+            # LOADER
             self.c.global_loader,
         ]
