98b9e20982c3e5a18faf88e7ffbcfccb8b33bc0a
Gianmario Iamoni <gianmarioiamoni1@gmail.com>
2026-05-06 04:33:15 +0200

    fix(ui): align outputs contract by including container components

--- app/ui/bindings/builders/ui_outputs_builder.py
+++ app/ui/bindings/builders/ui_outputs_builder.py
@@ -6,63 +6,45 @@ class UIOutputsBuilder:
         self.c = c
 
     def build(self):
-        print("=== OUTPUT ORDER ===")
-        for i, c in enumerate([
+
+        outputs = [
+            # 0 STATE
             self.c.state,
+            # 1-2 CONTAINERS (FONDAMENTALE)
             self.c.setup_container,
             self.c.interview_container,
+            # 3-7 SETUP INPUTS
             self.c.role_input,
             self.c.interview_type_input,
             self.c.company_input,
             self.c.language_input,
             self.c.start_button,
+            # 8 TITLE
             self.c.page_title,
+            # 9-10 HEADER
             self.c.question_counter,
             self.c.feedback_output,
+            # 11-13 DISPLAY
             self.c.written_display,
             self.c.coding_display,
             self.c.database_display,
+            # 14-15 REPORT
             self.c.final_feedback,
             self.c.report_output,
+            # 16-18 BUTTONS
             self.c.submit_button,
             self.c.retry_button,
             self.c.next_button,
+            # 19-21 EDITORS
             self.c.written_box,
             self.c.coding_box,
             self.c.database_box,
-            self.c.global_loader,
-        ]):
-            print(i, type(c), getattr(c, "_id", None))
-
-        return [
-            # STATE
-            self.c.state,
-            # SETUP INPUTS
-            self.c.role_input,
-            self.c.interview_type_input,
-            self.c.company_input,
-            self.c.language_input,
-            self.c.start_button,
-            # TITLE
-            self.c.page_title,
-            # HEADER
-            self.c.question_counter,
-            self.c.feedback_output,
-            # DISPLAY
-            self.c.written_display,
-            self.c.coding_display,
-            self.c.database_display,
-            # REPORT
-            self.c.final_feedback,
-            self.c.report_output,
-            # BUTTONS
-            self.c.submit_button,
-            self.c.retry_button,
-            self.c.next_button,
-            # EDITORS
-            self.c.written_box,
-            self.c.coding_box,
-            self.c.database_box,
-            # LOADER
+            # 22 LOADER
             self.c.global_loader,
         ]
+
+        print("=== OUTPUT ORDER ===")
+        for i, c in enumerate(outputs):
+            print(i, type(c), getattr(c, "_id", None))
+
+        return outputs
